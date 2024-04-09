ed87ede514576c3897b9773f37aaa4a819f72c40
George Brooks <hi@georgebrooks.dev>
2024-04-09 19:29:38 +0100

    API - basic routes and query filters working including 400 Not Found and 404 error.

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,9 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
-import express, {Express, Request, Response} from 'express';
+import express, {Express, NextFunction, Request, Response} from 'express';
 
+import {Track, TrackFilters} from 'types';
 import {tracks} from '../db/tracks.json' ;
-import {Track} from 'types';
 
 dotenv.config();
 
@@ -17,22 +17,49 @@ app.use(cors({origin: 'http://localhost:3000'}))
 
 
 /* ROUTES */
-/* GET ALL TRACKS */
-app.get("/", (request: Request, response: Response) => {
-    const allTracks: Track[] = tracks;
 
-    response.json({data: allTracks});
+app.get("/tracks", (req: Request, res: Response) => {
+
+    /* Destructure querystring filters */
+    const {artist, title}: TrackFilters = req.query;
+
+    /* Get ALL tracks */
+    let data: Track[] = tracks;
+
+    /* Given an artist name return _all_ associated tracks. */
+    if (artist && typeof artist === "string") {
+        data = data.filter((track) => track.artist.trim().toLowerCase().includes(artist.trim().toLowerCase()))
+    }
+    /* Why not filter by title too, just for kicks? */
+    if (title && typeof title === "string") {
+        data = data.filter((track) => track.title.trim().toLowerCase().includes(title.trim().toLowerCase()))
+    }
+    /* Return filtered tracks */
+    res.json({data});
 });
 
 /* Given a track id return the associated track. */
-app.get("/track", (request: Request, response: Response) => {
-    //TODO: get track_id from request, filter JSON, return individual track.
-})
-/* Given an artist name return _all_ associated tracks. */
-app.get("/artist", (request: Request, response: Response) => {
-    //TODO: get artist from request, filter JSON, return all artists' tracks.
+app.get("/tracks/:trackId", (req: Request, res: Response,) => {
+
+    /*Parse trackId param to integer */
+    const trackId = Number.parseInt(req.params.trackId)
+
+    /*400: Bad Request if trackId not valid */
+    if (typeof trackId !== 'number') {
+        res.status(400).json({error: "INVALID_TRACK_ID"})
+    }
+
+    let data: Track[] = tracks
+
+    /* Filter tracks by ID and return */
+    const result = data.filter((t) => t.id === trackId)
+
+    res.json({data: result})
 })
 
-//TODO: 404 error route
+/* 404 error route */
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({error: "File Not Found"});
+});
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
